7613114dc51a2e6bf5a22c7b97e2b630d46629bc
Evegenii <wkhan@ukr.net>
2018-07-06 21:25:21 +0300

    Added enum role to model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  ROLES = [:student, :curator, :teacher].freeze
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, length: { in: 2..30 }
   validates :surname, length: { in: 2..30 }
+
+  enum role: ROLES
 end
