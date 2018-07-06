dc24677a1718cc482085ef0156c1203a9d0ac587
Andrew <andrew.shyika@gmail.com>
2018-07-06 13:16:00 +0300

    Apply devise auth to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   validates :name, :length => { :in => 2..30 }
   validates :surname, :length => { :in => 2..30 }
 end
