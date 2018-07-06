86b3334ed5e4082aa12bda5091f510b6e19c55d9
Andrew <andrew.shyika@gmail.com>
2018-07-06 14:56:37 +0300

    Migration User syntax fix

--- db/migrate/20180706094632_create_users.rb
+++ db/migrate/20180706094632_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name, :default => '', :null => false
-      t.string :surname, :default => '', :null => false
-      t.string :email, :unique => true, :null => false
+      t.string :name, null: false, default: ''
+      t.string :surname, null: false, default: ''
+      t.string :email, unique: true, null: false, default: ''
 
       t.timestamps
     end
