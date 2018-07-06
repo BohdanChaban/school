0e6498ed74513563d468c9e48889fcd5a9272372
Andrew <andrew.shyika@gmail.com>
2018-07-06 22:50:52 +0300

    Rubocop fix

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,19 +1,20 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  it "is valid with valid attributes"
+RSpec.describe User, type: :model do
+  it 'is valid with valid attributes'
 
-  it "is not valid with too short name"
-  it "is not valid with too short surname"
-  it "is not valid with too long name"
-  it "is not valid with too long surname"
-  it "is not valid with duplicated email"
-  it "is not valid with invalid email"
+  it 'is not valid with too short name'
+  it 'is not valid with too short surname'
+  it 'is not valid with too long name'
+  it 'is not valid with too long surname'
+  it 'is not valid with duplicated email'
+  it 'is not valid with invalid email'
 
-  it "is not valid without a name"
-  it "is not valid without a surname"
-  it "is not valid without a email"
-  it "is not valid without a password"
-  it "is not valid without a password_confirmation"
-
+  it 'is not valid without a name'
+  it 'is not valid without a surname'
+  it 'is not valid without a email'
+  it 'is not valid without a password'
+  it 'is not valid without a password_confirmation'
 end
