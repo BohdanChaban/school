85a62197858c2f4538678784f86a12416b485860
Andrew <andrew.shyika@gmail.com>
2018-07-06 13:02:29 +0300

    User set fields constraints

--- db/migrate/20180706094632_create_users.rb
+++ db/migrate/20180706094632_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :surname
-      t.string :email
+      t.string :name, :default => '', :null => false
+      t.string :surname, :default => '', :null => false
+      t.string :email, :unique => true, :null => false
 
       t.timestamps
     end
