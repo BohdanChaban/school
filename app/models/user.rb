d0a61d74de3632c9dbf9639b6d080498739bf7ad
Evegenii <wkhan@ukr.net>
2018-07-07 15:01:40 +0300

    Replaced constant ROLES, added new validates rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  ROLES = %i[student curator teacher mentor].freeze
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, :surname, :email, presence: true
   validates :name, length: { in: 2..30 }
   validates :surname, length: { in: 2..30 }
+  validates :email, length: { in: 6..40 }
+  validates :approved, inclusion: { in: [true, false] }
 
+  ROLES = %i[student curator teacher mentor].freeze
   enum role: ROLES
 end
