ca5c318653431cc4552b07aaa6bc5073932721b4
Evegenii <wkhan@ukr.net>
2018-07-07 16:10:14 +0300

    Added new constans, added inclusion validates for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :name, :surname, :email, presence: true
-  validates :name, length: { in: 2..30 }
-  validates :surname, length: { in: 2..30 }
-  validates :email, length: { in: 6..40 }
-  validates :approved, inclusion: { in: [true, false] }
-
+  NAME_LENGTH_RANGE = 2..30
+  SURNAME_LENGTH_RANGE = NAME_LENGTH_RANGE
+  EMAIL_LENGTH_RANGE = 6..40
+  BOOLEN_VALUES = [true, false].freeze
   ROLES = %i[student curator teacher mentor].freeze
+
+  validates :name, :surname, :email, presence: true
+  validates :name, length: { in: NAME_LENGTH_RANGE }
+  validates :surname, length: { in: SURNAME_LENGTH_RANGE }
+  validates :email, length: { in: EMAIL_LENGTH_RANGE }
+  validates :approved, inclusion: { in: BOOLEN_VALUES }
+  validates :role, inclusion: { in: ROLES }
+
   enum role: ROLES
 end
