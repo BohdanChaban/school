42611f261e42ffc04805408856c415afbdee0354
Andrew <andrew.shyika@gmail.com>
2018-07-08 16:52:32 +0300

    Apply migrations to local db

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180706110113) do
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "surname", default: "", null: false
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
