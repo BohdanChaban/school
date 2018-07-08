521afcf143a9c478a4ae61d5e12bbb9eb6ff01e5
Andrew <andrew.shyika@gmail.com>
2018-07-08 17:15:32 +0300

    Set en as default for rspec passing

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module School
     config.load_defaults 5.1
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :ua]
-    config.i18n.default_locale = :ua
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
