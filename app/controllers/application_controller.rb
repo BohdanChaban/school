f8dd553e2aca59b828059e772088a55ebf498186
Andrew <andrew.shyika@gmail.com>
2018-07-08 17:12:27 +0300

    rubocop errors fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,19 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
 
-
   protected
 
   def set_locale
+    I18n.locale = validated_locale
+  end
+
+  def validated_locale
     locale = params[:locale].to_s.strip.to_sym
-    I18n.locale = I18n.available_locales.include?(locale) ?
-        locale :
-        I18n.default_locale
+    if I18n.available_locales.include?(locale)
+      locale
+    else
+      I18n.default_locale
+    end
   end
 
   def configure_permitted_parameters
