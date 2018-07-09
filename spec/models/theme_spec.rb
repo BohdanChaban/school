914f02b3f1ac5013eb6760641449032847090032
Andrew <andrew.shyika@gmail.com>
2018-07-09 19:58:08 +0300

    Theme model tests description

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Theme, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with valid attributes' do
+  end
+  it 'is invalid with long topic' do
+  end
+  it 'is invalid with short topic' do
+  end
+  it 'is invalid without topic' do
+  end
 end
