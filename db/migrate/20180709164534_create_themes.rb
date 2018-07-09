bd3b562ead7d6d623d0c4dc2a6f280075a60ad79
Andrew <andrew.shyika@gmail.com>
2018-07-09 19:47:35 +0300

    Add fields constraints to theme

--- db/migrate/20180709164534_create_themes.rb
+++ db/migrate/20180709164534_create_themes.rb
@@ -1,9 +1,9 @@
 class CreateThemes < ActiveRecord::Migration[5.1]
   def change
     create_table :themes do |t|
-      t.string :topic
-      t.text :description
-      t.text :links
+      t.string :topic, null: false, default: ''
+      t.text :description, null: false, default: ''
+      t.text :links, null: false, default: ''
       t.references :course, foreign_key: true
 
       t.timestamps
