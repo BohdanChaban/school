90588212559577e818eab0ce6208ab7570279904
Evegenii <wkhan@ukr.net>
2018-07-10 12:38:43 +0300

    Rebuild db schema with new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180708124342) do
+ActiveRecord::Schema.define(version: 20180709210834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180708124342) do
     t.string "topic", default: "", null: false
     t.string "summary"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.integer "number", null: false
+    t.string "parallel", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -48,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180708124342) do
     t.inet "last_sign_in_ip"
     t.integer "role", default: 0, null: false
     t.boolean "approved", default: false, null: false
+    t.integer "group_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
