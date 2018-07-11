fee14f18c71502295da560249c46cfcfc9b2491b
Andrew <andrew.shyika@gmail.com>
2018-07-11 10:57:34 +0300

    Migrate deleted course relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180708124342) do
+ActiveRecord::Schema.define(version: 20180709164534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,10 +30,18 @@ ActiveRecord::Schema.define(version: 20180708124342) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "themes", force: :cascade do |t|
+    t.string "topic", default: "", null: false
+    t.text "description", default: "", null: false
+    t.text "links", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "surname", default: "", null: false
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "encrypted_password", default: "", null: false
