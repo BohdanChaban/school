86014aaf93dea162caf02798805ddc8759d0bc66
Andrew <andrew.shyika@gmail.com>
2018-07-11 12:59:24 +0300

    fix deleted line in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20180710130033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,14 +34,6 @@
   create_table "subjects", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "image", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "themes", force: :cascade do |t|
-    t.string "topic", default: "", null: false
-    t.text "description", default: "", null: false
-    t.text "links", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
