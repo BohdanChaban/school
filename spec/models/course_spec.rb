d4b650eedd23a5fdd1eb4cb6d113107029dfa0ca
Andrew <andrew.shyika@gmail.com>
2018-07-13 16:27:01 +0300

    Describe tests contexts for course

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,5 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe Course, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with all valid attributes' do
+  end
+  it 'is valid without displayed attribute' do
+  end
+
+  it 'is invalid with displayed attribute type string' do
+  end
+  it 'is invalid with displayed attribute type int' do
+  end
+
+  it 'is invalid without group_id attribute' do
+  end
+  it 'is invalid with group_id attribute type string' do
+  end
+  it 'is invalid without subject_id attribute' do
+  end
+  it 'is invalid with subject_id attribute type string' do
+  end
+  it 'is invalid without user_id attribute' do
+  end
+  it 'is invalid with user_id attribute type string' do
+  end
 end
