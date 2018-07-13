ce0b9e8ff49a26a77fc2a1097ccb9e4e088981b2
Evegenii <wkhan@ukr.net>
2018-07-13 15:42:14 +0300

    Fixed Groups controller tests

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -54,18 +54,18 @@ RSpec.describe GroupsController, type: :controller do
   describe 'GET index' do
     it 'redirect to root path without auth.' do
       get :index
-      expect(response).to redirect_to root_path
+      expect(response).to redirect_to user_session_path
     end
 
     it 'redirect to show template with student role' do
       current_user = User.first
-      sign_in current_user
+      login_with current_user
       get :index
       expect(response).to redirect_to "/groups/#{current_user.group_id}"
     end
 
     it 'redner index template with role not student role' do
-      sign_in User.last
+      login_with User.last
       get :index
       expect(response).to render_template('index')
     end
@@ -74,13 +74,13 @@ RSpec.describe GroupsController, type: :controller do
   describe 'GET show' do
     it 'renders the show template with self student group id' do
       current_user = User.first
-      sign_in current_user
+      login_with current_user
       get :show, params: { id: current_user.group_id }
       expect(response).to render_template('show')
     end
 
     it 'renders the show template with not student role by some group id' do
-      sign_in User.last
+      login_with User.last
       get :show, params: { id: Group.first.id }
       expect(response).to render_template('show')
     end
@@ -89,7 +89,7 @@ RSpec.describe GroupsController, type: :controller do
       current_user = User.first
       other_user = User.second
       other_user[:group_id] = Group.second.id
-      sign_in current_user
+      login_with current_user
       get :show, params: { id: other_user.group_id }
       expect(response).to redirect_to root_path
     end
