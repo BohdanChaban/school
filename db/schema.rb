a7c8ecb753d1d6d9bbb3ab6dc8aef066d1113b90
Andrew <andrew.shyika@gmail.com>
2018-07-13 14:42:07 +0300

    Applied migration with course_it adding to theme

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712120402) do
+ActiveRecord::Schema.define(version: 20180713113459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20180712120402) do
     t.text "links", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "course_id"
   end
 
   create_table "users", force: :cascade do |t|
