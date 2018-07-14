b26258a4eb7d35a154c2683de5ffd9e9732d9df8
Andrew <andrew.shyika@gmail.com>
2018-07-14 18:30:07 +0300

    Migrate with course not null fields

--- db/migrate/20180711165728_add_column_to_course.rb
+++ db/migrate/20180711165728_add_column_to_course.rb
@@ -4,8 +4,8 @@ class AddColumnToCourse < ActiveRecord::Migration[5.1]
       t.boolean :displayed, null: false, default: true
     end
 
-    add_column :courses, :group_id, :bigint
-    add_column :courses, :subject_id, :bigint
-    add_column :courses, :user_id, :bigint
+    add_column :courses, :group_id, :bigint, null: false
+    add_column :courses, :subject_id, :bigint, null: false
+    add_column :courses, :user_id, :bigint, null: false
   end
 end
