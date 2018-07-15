1fb2b52c578636f2e296fa38f535148fc1852518
Andrew <andrew.shyika@gmail.com>
2018-07-15 15:39:31 +0300

    Add kind to achievement migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20180713132125) do
+ActiveRecord::Schema.define(version: 20180715123805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +22,7 @@ ActiveRecord::Schema.define(version: 20180713132125) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "lesson_id"
+    t.integer "kind", default: 0, null: false
     t.index ["lesson_id"], name: "index_achievements_on_lesson_id"
     t.index ["user_id"], name: "index_achievements_on_user_id"
   end
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 20180713132125) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "displayed", default: true, null: false
-    t.bigint "group_id", null: false
-    t.bigint "subject_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "group_id"
+    t.bigint "subject_id"
+    t.bigint "user_id"
   end
 
   create_table "groups", force: :cascade do |t|
