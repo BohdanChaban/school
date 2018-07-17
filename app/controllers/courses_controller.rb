480ff7791df74551264a42c8335b91de2d62892c
Yevhenii <wkhan@ukr.net>
2018-07-17 20:41:11 +0300

    Added to Course controller show action

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class CoursesController < ApplicationController
+  def show
+    @course = Course.includes("group","subject","user").where(id: params[:id])[0]
+    redirect_to root_path if redirect?
+  end
+
+  private
+
+  def redirect?
+    return true unless @course.displayed?
+    return true if current_user.student? && @course.group_id != current_user.group_id
+    false
+  end
+end
