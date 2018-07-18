8ba26eb6bb68a97456815ef9cb08216a797fe5ff
Yevhenii <wkhan@ukr.net>
2018-07-18 13:05:50 +0300

    Fixed bag with checking displayed course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,13 +3,14 @@
 class CoursesController < ApplicationController
   def show
     @course = Course.includes("group","subject","user").where(id: params[:id])[0]
+    p @course
     redirect_to root_path if redirect?
   end
 
   private
 
   def redirect?
-    return true unless @course.displayed?
+    return true unless @course.displayed
     return true if current_user.student? && @course.group_id != current_user.group_id
     false
   end
