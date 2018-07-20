236c6ddc5128b68f45d54397f8189e43c08e7dca
Andrew <andrew.shyika@gmail.com>
2018-07-20 13:21:44 +0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180715194933) do
-
+ActiveRecord::Schema.define(version: 20180716112615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,9 +54,9 @@ ActiveRecord::Schema.define(version: 20180715194933) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "displayed", default: true, null: false
-    t.bigint "group_id", null: false
-    t.bigint "subject_id", null: false
-    t.bigint "user_id", null: false
+    t.bigint "group_id"
+    t.bigint "subject_id"
+    t.bigint "user_id"
   end
 
   create_table "groups", force: :cascade do |t|
@@ -131,7 +130,7 @@ ActiveRecord::Schema.define(version: 20180715194933) do
     t.inet "last_sign_in_ip"
     t.integer "role", default: 0, null: false
     t.boolean "approved", default: false, null: false
-    t.integer "group_id"
+    t.integer "group_id", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
