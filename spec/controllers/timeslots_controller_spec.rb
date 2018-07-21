ca95877e34506caf48ae36d7511794e038015500
Yevhenii <wkhan@ukr.net>
2018-07-21 09:28:37 +0300

    Added commit to test

--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TimeslotsController, type: :controller do
   let(:timeslot) { FactoryBot.create(:valid_timeslot) }
   let(:student) { FactoryBot.create(:valid_user_for_registration, group_id: timeslot.group_id) }
 
-  it do
+  it 'returns http success if user is a student' do
     login_with student
     get :index
     expect(response).to have_http_status(:success)
