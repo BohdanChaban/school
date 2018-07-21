f03de0e25c75e327725d697b0e6d7a6b35ff87f0
Yevhenii <wkhan@ukr.net>
2018-07-21 09:24:49 +0300

    Added test for Timeslots controller

--- spec/controllers/timeslots_controller_spec.rb
+++ spec/controllers/timeslots_controller_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe TimeslotsController, type: :controller do
+  let(:timeslot) { FactoryBot.create(:valid_timeslot) }
+  let(:student) { FactoryBot.create(:valid_user_for_registration, group_id: timeslot.group_id) }
+
+  it do
+    login_with student
+    get :index
+    expect(response).to have_http_status(:success)
+  end
 end
