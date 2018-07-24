8c02a1f39add6b6976f0f954efa7c29dd06b6cf3
Bohdan Chaban <bohdan.chaban@itv.com>
2018-07-24 18:02:19 +0300

    Update journal ui, theme nested in courses

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,13 @@ Rails.application.routes.draw do
   resources :articles, only: %i[index show]
   resources :groups, only: %i[index show]
   resources :subjects, only: %i[index]
-  resources :courses, only: %i[index show]
+  resources :courses, only: %i[index show] do
+    resources :themes, except: %i[index show]
+  end
   resources :lessons
   resources :users, only: %i[index]
   resources :hometasks
   resources :timeslots, only: %i[index]
-  resources :themes, except: %i[index show]
   resources :achievements, except: %i[index show]
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 end
